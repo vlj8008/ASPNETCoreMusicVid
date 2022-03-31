4285a5e39fcb6fe406c4f584c74bb41d86a15482
Vicky Jones <vlj8008@gmail.com>
2022-03-31 16:38:54 -0400

    Re-organizing projects

---
+++ VSSubmission/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+const texts = ['Click', 'Below']
+let count = 0; // word in the array
+let index = 0; // individual letters
+let currentText = '';
+let letter = '';
+(function type() {
+
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index)
+
+    document.querySelector('.typing').textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 300);
+}());
